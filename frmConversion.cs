0d56a33d92d4f2ae2b9f85497cb433a0895f807d
usager <usager@G253-14>
2020-09-03 12:49:01 -0400

    Ajout commentaire

--- frmConversion.cs
+++ frmConversion.cs
@@ -16,5 +16,10 @@ namespace Conversion
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Commentaire
+        }
     }
 }
