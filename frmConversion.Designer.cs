4262ca859fd6c75f88c07e7b9b7469dbd83562c1
usager <usager@G253-14>
2020-09-03 12:44:40 -0400

    affiche

--- frmConversion.Designer.cs
+++ frmConversion.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Conversion
 {
-    partial class Form1
+    partial class frmConversion
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmConversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "frmConversion";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- frmConversion.cs
+++ frmConversion.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Conversion
 {
-    public partial class Form1 : Form
+    public partial class frmConversion : Form
     {
-        public Form1()
+        public frmConversion()
         {
             InitializeComponent();
         }
